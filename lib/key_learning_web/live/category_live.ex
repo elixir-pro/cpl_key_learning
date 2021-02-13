37f5159c787fa1cca263fd60dfa793654c7b71df
Gustavo Oliveira <g.92oliveira@gmail.com>
2021-02-13 17:48:28 -0300

    start liveview search

--- lib/key_learning_web/live/category_live.ex
+++ lib/key_learning_web/live/category_live.ex
@@ -1,9 +1,11 @@
 defmodule KeyLearningWeb.CategoryLive do
   use KeyLearningWeb, :live_view
+  alias KeyLearning.School
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+     socket
+     |> assign(courses: School.list_courses())}
   end
-
 end
